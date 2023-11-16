128e86a504652dfd0da85322fa6ecd77c1a9781f
c0de4un <code4un@yandex.ru>
2023-11-16 09:59:59 +0400

    #9 ref: Separate API endpoints to class

--- main.go
+++ main.go
@@ -2,24 +2,12 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
 )
 
-func handleNotFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusNotFound)
-
-	fmt.Fprint(w, "{}")
-}
-
 func main() {
-	router := chi.NewRouter()
-	router.NotFound(handleNotFound)
-
 	fmt.Print("Starting server on :3000 ...")
-	err := http.ListenAndServe(":3000", router)
+
+	err := NewAPIServer(":3000").Run()
 	if err != nil {
 		panic(err)
 	}
